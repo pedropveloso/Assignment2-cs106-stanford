fc48d118f83bce2c9316d1e5199d23f8fdc52906
S <s>
2012-11-03 12:13:33 +0000

    {"type":"TI", "runs":"3"}

--- Target.java
+++ Target.java
@@ -10,8 +10,43 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class Target extends GraphicsProgram {	
+
+public class Target extends GraphicsProgram {
+	
+	/** Radius of the outer circle in pixels*/
+	private static final double OUTER_CIRCLE_RADIUS = 72.0;
+	
+	/** Radius of the white circle in pixels*/
+	private static final double MIDDLE_CIRCLE_RADIUS = (0.65 * 72);
+
+	/** Radius of the inner circle in pixels*/
+	private static final double INNER_CIRCLE_RADIUS = (0.3 * 72);
+	
+	
 	public void run() {
-		/* You fill this in. */
+		double outerCircleDiameter = 2 * OUTER_CIRCLE_RADIUS;
+		double middleCircleDiameter = 2 * MIDDLE_CIRCLE_RADIUS;
+		double innerCircleDiameter = 2 * INNER_CIRCLE_RADIUS;
+		double xOuterCircle = (getWidth() - outerCircleDiameter) / 2;
+		double xMiddleCircle = (getWidth() - middleCircleDiameter) / 2;
+		double xInnerCircle = (getWidth() - innerCircleDiameter) / 2;
+		double yOuterCircle = (getHeight() - outerCircleDiameter) / 2;
+		double yMiddleCircle = (getHeight() - middleCircleDiameter) / 2;
+		double yInnerCircle = (getHeight() - innerCircleDiameter) / 2;
+		GOval outCircle = new GOval(xOuterCircle,yOuterCircle,outerCircleDiameter,outerCircleDiameter);
+		GOval middleCircle = new GOval(xMiddleCircle,yMiddleCircle,middleCircleDiameter,middleCircleDiameter);
+		GOval centreCircle = new GOval(xInnerCircle,yInnerCircle,innerCircleDiameter,innerCircleDiameter);
+		outCircle.setColor(Color.RED);
+		middleCircle.setColor(Color.WHITE);
+		centreCircle.setColor(Color.RED);
+		outCircle.setFilled(true);
+		middleCircle.setFilled(true);
+		centreCircle.setFilled(true);
+		outCircle.setFillColor(Color.RED);
+		middleCircle.setFillColor(Color.WHITE);
+		centreCircle.setFillColor(Color.RED);
+		add (outCircle);
+		add (middleCircle);
+		add (centreCircle);
 	}
 }
