08646b2ef34cfa8a471c538494fed03a85a05dc6
S <s>
2012-11-05 22:20:13 +0000

    {"type":"TI", "runs":"0"}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -11,8 +11,45 @@ import acm.program.*;
 import java.awt.*;
 
 public class ProgramHierarchy extends GraphicsProgram {	
+	
+	/*Width of the class boxes*/
+	private static final int BOX_WIDTH = 120;
+	
+	/*Height of the class box*/
+	private static final int BOX_HEIGHT = 30;
+	
+	/*Y size of the connecting lines*/
+	private static final int LINE_SIZE = 30;
+	
+	/*Distance between boxes in the bottom row*/
+	private static final int DISTANCE_BOXES = 10;
+	
 	public void run() {
-		/* You fill this in. */
+		double xProgramBox = (double) (getWidth()-BOX_WIDTH) / 2;
+		double yProgramBox = (double) (getHeight() - (2 * BOX_HEIGHT + LINE_SIZE)) / 2;
+		double xGraphicsProgramBox = (double) (getWidth() - (3 * BOX_WIDTH + 2 * DISTANCE_BOXES)) / 2;
+		double yBottomRow = yProgramBox + BOX_HEIGHT + LINE_SIZE;
+		double xDialogueProgramBox = xGraphicsProgramBox + 2 * BOX_WIDTH + 2 * DISTANCE_BOXES;
+		double xProgram = 1.0;
+		double yProgram = yProgramBox;
+		GRect programBox = new GRect (xProgramBox, yProgramBox, BOX_WIDTH, BOX_HEIGHT);
+		GRect graphicsProgramBox = new GRect (xGraphicsProgramBox, yBottomRow, BOX_WIDTH, BOX_HEIGHT);
+		GRect consoleProgramBox = new GRect (xProgramBox, yBottomRow, BOX_WIDTH, BOX_HEIGHT);
+		GRect dialogProgramBox = new GRect (xDialogueProgramBox, yBottomRow, BOX_WIDTH, BOX_HEIGHT);
+		GLabel program = new GLabel ("Program", xProgram, yProgram);
+		xProgram = xProgramBox + (BOX_WIDTH - program.getWidth()) / 2;
+		add (programBox);
+		add (graphicsProgramBox);
+		add (consoleProgramBox);
+		add (dialogProgramBox);
+		add (program);
+		//add (graphicsProgram);
+		//add (consoleProgram);
+		//add (DialogueProgram);
+		//add (connectingLine1);
+		//add (connectingLine2);
+		//add (connectingLine3);
+		
 	}
 }
 
