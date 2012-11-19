b3881e3863b5924159f1ad31791ffa1c2fdba222
S <s>
2012-11-19 23:08:56 +0000

    {"type":"TI", "runs":"4"}

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,16 +4,16 @@ public class Fibonacci extends ConsoleProgram{
 	public void run(){
 		println("This program lists the Fibonacci sequence.");
 		int x = 0;
-		int y;
+		int y=0;
 		while (x <10000) {
 			println(x);
-			y=x;
 			if (x==0) {
 				x++;
 			}else{
 				x+=y;
 			}
-			
+			y=x;
+
 			
 		}
 		
