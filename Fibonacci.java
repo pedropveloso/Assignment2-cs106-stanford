2804a8fd7fb32e282550f916cdaae19b291926cd
S <s>
2012-11-19 23:03:52 +0000

    {"type":"TI", "runs":"0"}

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,4 +1,22 @@
+import acm.program.*;
 
-public class Fibonacci {
+public class Fibonacci extends ConsoleProgram{
+	public void run(){
+		println("This program lists the Fibonacci sequence.");
+		int x = 0;
+		int y;
+		while (x <10000) {
+			println(x);
+			y=x;
+			if (x==0) {
+				x++;
+			}else{
+				x+=y;
+			}
+			
+			
+		}
+		
+	}
 
 }
