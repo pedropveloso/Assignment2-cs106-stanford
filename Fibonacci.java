be6e75bbad2a0b7c9ce7a322c87fede73ead6858
S <s>
2012-11-19 23:16:03 +0000

    {"type":"TI", "runs":"1"}

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,17 +3,13 @@ import acm.program.*;
 public class Fibonacci extends ConsoleProgram{
 	public void run(){
 		println("This program lists the Fibonacci sequence.");
-		int x = 0;
-		int y=0;
-		while (x <10000) {
-			println(x);
-			if (x==0) {
-				x++;
-			}else{
-				x+=y;
-			}
-			y=x;
-
+		int t1 = 0;
+		int t2 = 0;
+		while (t1<10000) {
+			int t3 = t1+t2;
+			println(t1);
+			t2=t1;
+			t2=t3;
 			
 		}
 		
